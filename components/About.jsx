508e25f0970e58204aae1b7eaad4842b17844663
masudranashawon <mohammadmasudrana3396@gmail.com>
2023-07-06 20:22:31 +0600

    about designed done

--- components/About.jsx
+++ components/About.jsx
@@ -7,23 +7,27 @@ const About = () => {
       <SectionTitle title='About me' />
 
       {/* ABOUT */}
-      <div className='about-content mt-20 relative'>
-        <div className='about-image w-full lg:w-[40%] overflow-hidden'>
+      <div className='about-content mt-20 relative h-auto lg:h-[70rem] xl:h-[65rem] 2xl:h-[60rem] overflow-hidden'>
+        <div className='about-image w-full lg:w-[40%] overflow-hidden rounded-2xl'>
           <Image
             src='https://res.cloudinary.com/drgxflcsb/image/upload/v1676030149/Personal%20Portfolio/masud-image1_rejzqv.jpg'
             alt='Masud Rana Shawon'
             width={500}
             height={500}
-            className='w-full h-full object-cover'
+            className='w-full h-full object-cover hover:scale-[1.1] duration-[2000ms]'
           />
         </div>
+
         {/* DETAILS */}
-        <div className='about-details w-full lg:w-[63%] lg:absolute right-0 top-[5rem]'>
-          <h2>
-            <span>Hi there,</span> <span></span>I&apos;m Masud Rana Shawon - a
-            passionate and innovative web developer.
+        <div className='about-details w-full lg:w-[63%] lg:absolute right-0 top-[10rem] radial backdrop-blur-xl p-10 rounded-2xl space-y-5'>
+          <h2 className='text-[1rem] md:text-[1.8rem]  lg:text-[2.1rem] xl:text-[2.6rem] font-bold leading-snug'>
+            <span className='block mb-5'>Hello,</span> My name is Masud Rana
+            Shawon i&apos;m a<span className='text-accent'> passionate </span>
+            and <span className='text-accent'>innovative</span> web developer
+            with a strong focus on
+            <span className='text-accent'> mern stack</span>.
           </h2>
-          <p>
+          <p className='text-[1.5rem] leading-[1.2]'>
             Welcome to my personal portfolio! I have a deep love for JavaScript,
             and I am constantly immersing myself in learning and exploring its
             intricacies. It is a language that fascinates me with its
