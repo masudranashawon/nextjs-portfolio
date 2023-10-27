fa9d69d56281a3c4a7a68dc79a053e2f1e3e8601
masudranashawon <mohammadmasudrana3396@gmail.com>
2023-10-27 19:33:55 +0600

    project-animation added

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -8,16 +8,16 @@ const ProjectCard = ({ project }) => {
   const [scroll, setScroll] = useState(false);
 
   return (
-    <div
-      data-aos='fade-up'
-      className='relative lg:grid lg:grid-cols-12 w-full lg:h-[30rem] p-5 lg:p-0 bg-white lg:bg-transparent rounded-lg lg:rounded-none space-y-5 lg:space-y-0 lg:items-center'
-    >
+    <div className='relative lg:grid lg:grid-cols-12 w-full lg:h-[30rem] p-5 lg:p-0 bg-white lg:bg-transparent rounded-lg lg:rounded-none space-y-5 lg:space-y-0 lg:items-center'>
       {/* PROJECT IMAGE */}
-      <div className='z-10 lg:py-4 lg:absolute lg:grid grid-cols-12 w-full h-full content-center'>
+      <div className='z-10 lg:py-4 lg:absolute lg:grid grid-cols-12 w-full h-full content-center overflow-hidden'>
         <div
+          data-aos='fade-right'
+          data-aos-duration='1000'
+          data-aos-easing='ease-in-out'
           className={clsx(
             !project.revarse
-              ? "col-span-7 "
+              ? "col-span-7"
               : "xl:col-span-7 xl:col-start-6 col-start-5 col-span-8",
             "relative rounded w-full overflow-hidden h-[28rem] :hover:shadow-xl hover:border hover:border-accent/5 bg-dark/20 duration-500"
           )}
@@ -47,12 +47,13 @@ const ProjectCard = ({ project }) => {
       </div>
 
       {/* PROJECT DETAILS */}
-      <div className='lg:absolute lg:left-0 lg:top-0 lg:py-4 lg:grid lg:grid-cols-12 w-full h-full content-center items-center'>
+      <div className='project-details lg:absolute lg:left-0 lg:top-0 lg:py-4 lg:grid lg:grid-cols-12 w-full h-full content-center items-center overflow-hidden'>
         <div
           className={clsx(
             !project.revarse
-              ? "xl:col-span-6 xl:col-start-7 col-start-5 col-span-8 flex flex-col items-start lg:items-end space-y-3"
-              : "col-span-6 flex flex-col items-start lg:items-start space-y-3"
+              ? "xl:col-span-6 xl:col-start-7 col-start-5 col-span-8  lg:items-end"
+              : "col-span-6 lg:items-start ",
+            "flex flex-col items-start space-y-3"
           )}
         >
           <div
@@ -62,27 +63,58 @@ const ProjectCard = ({ project }) => {
             )}
           >
             {!project.revarse ? (
-              <h2 className='text-dark font-bold text-2xl leading-relaxed tracking-wider flex gap-2 items-center'>
-                <span className='font-normal italic text-lg'>
-                  ({project.completeDate})
-                </span>
-                <Link href={project.liveLink} target='_blank'>
-                  {project.title}
-                </Link>
-              </h2>
+              <div className='overflow-hidden'>
+                <h2
+                  data-aos='slide-down'
+                  data-aos-delay='500'
+                  data-aos-duration='1000'
+                  data-aos-easing='ease-in-out'
+                  className='text-dark font-bold text-2xl leading-relaxed tracking-wider flex gap-2 items-center'
+                >
+                  <span className='font-normal italic text-lg'>
+                    ({project.completeDate})
+                  </span>
+                  <Link href={project.liveLink} target='_blank'>
+                    {project.title}
+                  </Link>
+                </h2>
+              </div>
             ) : (
-              <h2 className='text-dark font-bold text-2xl leading-relaxed tracking-wider flex gap-2 items-center'>
-                <Link href={project.liveLink} target='_blank'>
-                  {project.title}
-                </Link>
-                <span className='font-normal italic text-lg'>
-                  ({project.completeDate})
-                </span>
-              </h2>
+              <div className='overflow-hidden'>
+                <h2
+                  data-aos='slide-down'
+                  data-aos-delay='500'
+                  data-aos-duration='1000'
+                  data-aos-easing='ease-in-out'
+                  className='text-dark font-bold text-2xl leading-relaxed tracking-wider flex gap-2 items-center'
+                >
+                  <Link href={project.liveLink} target='_blank'>
+                    {project.title}
+                  </Link>
+                  <span className='font-normal italic text-lg'>
+                    ({project.completeDate})
+                  </span>
+                </h2>
+              </div>
             )}
-            <span className='text-accent text-base'>{project.category}</span>
+            <div className='w-full overflow-hidden'>
+              <span
+                data-aos='slide-up'
+                data-aos-delay='800'
+                data-aos-duration='1000'
+                data-aos-easing='ease-in-out'
+                className='text-accent text-base block'
+              >
+                {project.category}
+              </span>
+            </div>
           </div>
+
           <div
+            data-aos='fade-left'
+            data-aos-delay='1200'
+            data-aos-duration='1000'
+            data-aos-easing='ease-in-out'
             className={clsx(
               !project.revarse ? "bg-gradient-to-tr" : "bg-gradient-to-bl",
               "to-teal-700 from-teal-600 w-full rounded-md p-3 lg:p-6 z-10"
@@ -104,53 +136,89 @@ const ProjectCard = ({ project }) => {
             )}
           >
             {project.tools.map((tool, i) => (
-              <span key={i} className='z-10 font-medium'>
+              <span
+                data-aos='zoom-out'
+                data-aos-delay='1600'
+                data-aos-duration='1000'
+                data-aos-easing='ease-out-cubic'
+                data-aos-anchor-placement='top center'
+                className='z-10 font-medium inline-block'
+                key={i}
+              >
                 {tool}
               </span>
             ))}
           </div>
 
           <div className='z-10 flex space-x-5 font-semibold'>
-            <Link
-              href={project.liveLink}
-              target={"_blank"}
-              className='flex gap-1 items-center hover:text-accent duration-500 whitespace-nowrap'
-            >
-              <span>Live Demo</span>
-              <FiExternalLink />
-            </Link>
-
-            {project.fullStackLink && (
+            <div className='overflow-hidden'>
               <Link
-                href={project.fullStackLink}
+                data-aos='slide-up'
+                data-aos-delay='1800'
+                data-aos-duration='1000'
+                data-aos-easing='ease-in-out'
+                data-aos-anchor-placement='top center'
+                href={project.liveLink}
                 target={"_blank"}
                 className='flex gap-1 items-center hover:text-accent duration-500 whitespace-nowrap'
               >
-                <span>Full-Stack</span>
-                <FiGithub />
+                <span>Live Demo</span>
+                <FiExternalLink />
               </Link>
+            </div>
+
+            {project.fullStackLink && (
+              <div className='overflow-hidden'>
+                <Link
+                  data-aos='slide-up'
+                  data-aos-delay='2000'
+                  data-aos-duration='1000'
+                  data-aos-easing='ease-in-out'
+                  data-aos-anchor-placement='top center'
+                  href={project.fullStackLink}
+                  target={"_blank"}
+                  className='flex gap-1 items-center hover:text-accent duration-500 whitespace-nowrap'
+                >
+                  <span>Full-Stack</span>
+                  <FiGithub />
+                </Link>
+              </div>
             )}
 
             {project.frontEndLink && (
-              <Link
-                href={project.frontEndLink}
-                target={"_blank"}
-                className='flex gap-1 items-center hover:text-accent duration-500 whitespace-nowrap'
-              >
-                <span>Front-End</span>
-                <FiGithub />
-              </Link>
+              <div className='overflow-hidden'>
+                <Link
+                  data-aos='slide-up'
+                  data-aos-delay='2200'
+                  data-aos-duration='1000'
+                  data-aos-easing='ease-in-out'
+                  data-aos-anchor-placement='top center'
+                  href={project.frontEndLink}
+                  target={"_blank"}
+                  className='flex gap-1 items-center hover:text-accent duration-500 whitespace-nowrap'
+                >
+                  <span>Front-End</span>
+                  <FiGithub />
+                </Link>
+              </div>
             )}
 
             {project.backEndLink && (
-              <Link
-                href={project.backEndLink}
-                target={"_blank"}
-                className='flex gap-1 items-center hover:text-accent duration-500 whitespace-nowrap'
-              >
-                <span>Back-End</span>
-                <FiGithub />
-              </Link>
+              <div className='overflow-hidden'>
+                <Link
+                  data-aos='slide-up'
+                  data-aos-delay='2400'
+                  data-aos-duration='1000'
+                  data-aos-easing='ease-in-out'
+                  data-aos-anchor-placement='top center'
+                  href={project.backEndLink}
+                  target={"_blank"}
+                  className='flex gap-1 items-center hover:text-accent duration-500 whitespace-nowrap'
+                >
+                  <span>Back-End</span>
+                  <FiGithub />
+                </Link>
+              </div>
             )}
           </div>
         </div>
