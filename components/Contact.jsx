2fb2e4256f000688bbe7cf51f39c83efa1ce0db2
masudranashawon <mohammadmasudrana3396@gmail.com>
2023-10-28 20:45:15 +0600

    horizontal scroll bar issue fixed

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -12,236 +12,238 @@ import SectionTitle from "./SectionTitle";
 
 const Contact = () => {
   return (
-    <section id='contact' className='wrapper section-padding overflow-hidden'>
-      <SectionTitle title='Contact me' />
+    <section id='contact' className='w-[100%] section-padding overflow-hidden'>
+      <div className='wrapper'>
+        <SectionTitle title='Contact me' />
 
-      <div className='mt-20 grid lg:grid-cols-2 gap-10 overflow-hidden'>
         {/* CONTACT DETAILS */}
-        <div className='contact-details'>
-          <div className='overflow-hidden'>
-            <p
-              data-aos='slide-down'
-              data-aos-duration='1000'
-              data-aos-delay='1000'
-              data-aos-easing='ease-in-out'
-              className='text-accent tracking-wider'
-            >
-              Get in touch
-            </p>
-          </div>
-          <div className='overflow-hidden'>
-            <h2
-              data-aos='slide-down'
-              data-aos-duration='1000'
-              data-aos-delay='1200'
-              data-aos-easing='ease-in-out'
-              className='lg:text-4xl text-3xl font-bold leading-normal'
-            >
-              Unlocking the Next Level of Possibilities ! Let&apos;s Connect.
-            </h2>
-          </div>
-
-          <address className='not-italic space-y-8 mt-10'>
-            <div className='social-network space-y-2 overflow-hidden'>
-              <h4
-                data-aos='slide-right'
+        <div className='mt-20 grid lg:grid-cols-2 gap-10 overflow-hidden'>
+          <div className='contact-details'>
+            <div className='overflow-hidden'>
+              <p
+                data-aos='slide-down'
                 data-aos-duration='1000'
-                data-aos-delay='1300'
+                data-aos-delay='1000'
                 data-aos-easing='ease-in-out'
-                className='text-xl font-bold'
+                className='text-accent tracking-wider'
               >
-                Social network
-              </h4>
-
-              {/* SOCIAL */}
-              <div className='social-network flex gap-5 text-2xl text-accent overflow-hidden'>
-                <div
-                  data-aos='zoom-in'
-                  data-aos-delay='1900'
-                  data-aos-duration='1000'
-                  data-aos-easing='ease-out-cubic'
-                  className='github'
-                >
-                  <Link
-                    href='https://github.com/masudranashawon'
-                    target='_blank'
-                    className='text-3xl hover:text-dark duration-500'
-                  >
-                    <BiLogoGithub />
-                  </Link>
-                </div>
-                <div
-                  data-aos='zoom-in'
-                  data-aos-delay='2000'
-                  data-aos-duration='1000'
-                  data-aos-easing='ease-out-cubic'
-                  className='leetcode'
-                >
-                  <Link
-                    href='https://leetcode.com/masudranashawon'
-                    target='_blank'
-                    className='text-3xl hover:text-dark duration-500'
-                  >
-                    <SiLeetcode />
-                  </Link>
-                </div>
-                <div
-                  data-aos='zoom-in'
-                  data-aos-delay='2100'
-                  data-aos-duration='1000'
-                  data-aos-easing='ease-out-cubic'
-                  className='linkedin'
-                >
-                  <Link
-                    href='https://www.linkedin.com/in/masudranashawon1'
-                    target='_blank'
-                    className='text-3xl hover:text-dark duration-500'
-                  >
-                    <BiLogoLinkedin />
-                  </Link>
-                </div>
-                <div
-                  data-aos='zoom-in'
-                  data-aos-delay='2200'
-                  data-aos-duration='1000'
-                  data-aos-easing='ease-out-cubic'
-                  className='twitter'
-                >
-                  <Link
-                    href='https://twitter.com/MasudRanaShawo1'
-                    target='_blank'
-                    className='text-3xl hover:text-dark duration-500'
-                  >
-                    <BiLogoTwitter />
-                  </Link>
-                </div>
-                <div
-                  data-aos='zoom-in'
-                  data-aos-delay='2300'
-                  data-aos-duration='1000'
-                  data-aos-easing='ease-out-cubic'
-                  className='facebook'
-                >
-                  <Link
-                    href='https://www.facebook.com/masudranashawon786'
-                    target='_blank'
-                    className='text-3xl hover:text-dark duration-500'
-                  >
-                    <BiLogoFacebook />
-                  </Link>
-                </div>
-              </div>
+                Get in touch
+              </p>
+            </div>
+            <div className='overflow-hidden'>
+              <h2
+                data-aos='slide-down'
+                data-aos-duration='1000'
+                data-aos-delay='1200'
+                data-aos-easing='ease-in-out'
+                className='lg:text-4xl text-3xl font-bold leading-normal'
+              >
+                Unlocking the Next Level of Possibilities ! Let&apos;s Connect.
+              </h2>
             </div>
 
-            <div className='mail overflow-hidden'>
-              <h4
-                data-aos='slide-right'
-                data-aos-duration='1000'
-                data-aos-delay='1400'
-                data-aos-easing='ease-in-out'
-                className='text-xl font-bold space-y-2'
-              >
-                Talks via mail!
-              </h4>
+            <address className='not-italic space-y-8 mt-10'>
+              <div className='social-network space-y-2 overflow-hidden'>
+                <h4
+                  data-aos='slide-right'
+                  data-aos-duration='1000'
+                  data-aos-delay='1300'
+                  data-aos-easing='ease-in-out'
+                  className='text-xl font-bold'
+                >
+                  Social network
+                </h4>
+
+                {/* SOCIAL */}
+                <div className='social-network flex gap-5 text-2xl text-accent overflow-hidden'>
+                  <div
+                    data-aos='zoom-in'
+                    data-aos-delay='1900'
+                    data-aos-duration='1000'
+                    data-aos-easing='ease-out-cubic'
+                    className='github'
+                  >
+                    <Link
+                      href='https://github.com/masudranashawon'
+                      target='_blank'
+                      className='text-3xl hover:text-dark duration-500'
+                    >
+                      <BiLogoGithub />
+                    </Link>
+                  </div>
+                  <div
+                    data-aos='zoom-in'
+                    data-aos-delay='2000'
+                    data-aos-duration='1000'
+                    data-aos-easing='ease-out-cubic'
+                    className='leetcode'
+                  >
+                    <Link
+                      href='https://leetcode.com/masudranashawon'
+                      target='_blank'
+                      className='text-3xl hover:text-dark duration-500'
+                    >
+                      <SiLeetcode />
+                    </Link>
+                  </div>
+                  <div
+                    data-aos='zoom-in'
+                    data-aos-delay='2100'
+                    data-aos-duration='1000'
+                    data-aos-easing='ease-out-cubic'
+                    className='linkedin'
+                  >
+                    <Link
+                      href='https://www.linkedin.com/in/masudranashawon1'
+                      target='_blank'
+                      className='text-3xl hover:text-dark duration-500'
+                    >
+                      <BiLogoLinkedin />
+                    </Link>
+                  </div>
+                  <div
+                    data-aos='zoom-in'
+                    data-aos-delay='2200'
+                    data-aos-duration='1000'
+                    data-aos-easing='ease-out-cubic'
+                    className='twitter'
+                  >
+                    <Link
+                      href='https://twitter.com/MasudRanaShawo1'
+                      target='_blank'
+                      className='text-3xl hover:text-dark duration-500'
+                    >
+                      <BiLogoTwitter />
+                    </Link>
+                  </div>
+                  <div
+                    data-aos='zoom-in'
+                    data-aos-delay='2300'
+                    data-aos-duration='1000'
+                    data-aos-easing='ease-out-cubic'
+                    className='facebook'
+                  >
+                    <Link
+                      href='https://www.facebook.com/masudranashawon786'
+                      target='_blank'
+                      className='text-3xl hover:text-dark duration-500'
+                    >
+                      <BiLogoFacebook />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+
               <div className='mail overflow-hidden'>
-                <Link
-                  data-aos='slide-down'
+                <h4
+                  data-aos='slide-right'
                   data-aos-duration='1000'
-                  data-aos-delay='2300'
+                  data-aos-delay='1400'
                   data-aos-easing='ease-in-out'
-                  data-aos-anchor-placement='top center'
-                  href='mailto:masudranashawon1@gmail.com'
-                  target='_blank'
-                  className='email text-dark/80 lowercase text-lg line-flow inline-block'
+                  className='text-xl font-bold space-y-2'
                 >
-                  masudranashawon1@gmail.com
-                </Link>
+                  Talks via mail!
+                </h4>
+                <div className='mail overflow-hidden'>
+                  <Link
+                    data-aos='slide-down'
+                    data-aos-duration='1000'
+                    data-aos-delay='2300'
+                    data-aos-easing='ease-in-out'
+                    data-aos-anchor-placement='top center'
+                    href='mailto:masudranashawon1@gmail.com'
+                    target='_blank'
+                    className='email text-dark/80 lowercase text-lg line-flow inline-block'
+                  >
+                    masudranashawon1@gmail.com
+                  </Link>
+                </div>
               </div>
-            </div>
 
-            <div className='address overflow-hidden'>
-              <h4
-                data-aos='slide-right'
-                data-aos-duration='1000'
-                data-aos-delay='1500'
-                data-aos-easing='ease-in-out'
-                data-aos-anchor='.email'
-                className='text-xl font-bold space-y-2'
-              >
-                Address
-              </h4>
-              <div className='overflow-hidden'>
-                <Link
-                  data-aos='slide-down'
+              <div className='address overflow-hidden'>
+                <h4
+                  data-aos='slide-right'
                   data-aos-duration='1000'
-                  data-aos-delay='2400'
+                  data-aos-delay='1500'
                   data-aos-easing='ease-in-out'
-                  data-aos-anchor-placement='top center'
                   data-aos-anchor='.email'
-                  href='https://maps.app.goo.gl/WCRRkontEbpGqQqR8'
-                  target='_blank'
-                  className='text-dark/80 text-lg line-flow inline-block'
+                  className='text-xl font-bold space-y-2'
                 >
-                  Dhaka, Bangladesh
-                </Link>
+                  Address
+                </h4>
+                <div className='overflow-hidden'>
+                  <Link
+                    data-aos='slide-down'
+                    data-aos-duration='1000'
+                    data-aos-delay='2400'
+                    data-aos-easing='ease-in-out'
+                    data-aos-anchor-placement='top center'
+                    data-aos-anchor='.email'
+                    href='https://maps.app.goo.gl/WCRRkontEbpGqQqR8'
+                    target='_blank'
+                    className='text-dark/80 text-lg line-flow inline-block'
+                  >
+                    Dhaka, Bangladesh
+                  </Link>
+                </div>
               </div>
-            </div>
-          </address>
-        </div>
+            </address>
+          </div>
 
-        {/* CONTACT FORM */}
-        <div className='overflow-hidden'>
-          <div
-            data-aos='slide-left'
-            data-aos-duration='1000'
-            data-aos-delay='2500'
-            data-aos-easing='ease-in-out'
-            className='contact-form bg-gradient-to-tl to-teal-600 from-teal-400 p-10 rounded-2xl shadow-md'
-          >
-            <form className='flex flex-col pt-10 gap-10' autoComplete='off'>
-              <div className='form-control w-full'>
-                <input
-                  type='text'
-                  name='fullname'
-                  placeholder='FULL NAME'
-                  required
-                  className='w-full bg-transparent outline-none border-b border-light/40 text-light placeholder:text-light/80 text-xl pb-4 capitalize focus-within:border-light duration-500'
-                />
-              </div>
-              <div className='form-control w-full'>
-                <input
-                  type='email'
-                  name='email'
-                  placeholder='EMAIL ADDRESS'
-                  required
-                  className='w-full bg-transparent outline-none border-b border-light/40 text-light placeholder:text-light/80 text-xl pb-4 focus-within:border-light duration-500'
-                />
-              </div>
-              <div className='form-control w-full'>
-                <textarea
-                  name='message'
-                  rows='4'
-                  placeholder='MESSAGE'
-                  required
-                  className='w-full bg-transparent outline-none border-b border-light/40 text-light placeholder:text-light/80 text-xl pb-4 resize-none focus-within:border-light duration-500'
-                ></textarea>
-              </div>
-              <div
-                data-aos='zoom-in'
-                data-aos-duration='1000'
-                data-aos-delay='2600'
-                data-aos-easing='ease-out-cubic'
-                data-aos-anchor-placement='top center'
-                className='flex items-center justify-center'
-              >
-                <button
-                  type='submit'
-                  className='submit px-5 py-3 rounded-full text-light text-xl lg:text-2xl whitespace-nowrap bg-dark/70 hover:bg-dark/80 hover:shadow-lg duration-500 uppercase'
+          {/* CONTACT FORM */}
+          <div className='overflow-hidden w-full'>
+            <div
+              data-aos='slide-left'
+              data-aos-duration='1000'
+              data-aos-delay='2500'
+              data-aos-easing='ease-in-out'
+              className='contact-form bg-gradient-to-tl to-teal-600 from-teal-400 p-10 rounded-2xl shadow-md'
+            >
+              <form className='flex flex-col gap-10 mt-8' autoComplete='off'>
+                <div className='form-control w-full'>
+                  <input
+                    type='text'
+                    name='fullname'
+                    placeholder='FULL NAME'
+                    required
+                    className='w-full bg-transparent outline-none border-b border-light/40 text-light placeholder:text-light/80 text-xl pb-4 capitalize focus-within:border-light duration-500'
+                  />
+                </div>
+                <div className='form-control w-full'>
+                  <input
+                    type='email'
+                    name='email'
+                    placeholder='EMAIL ADDRESS'
+                    required
+                    className='w-full bg-transparent outline-none border-b border-light/40 text-light placeholder:text-light/80 text-xl pb-4 focus-within:border-light duration-500'
+                  />
+                </div>
+                <div className='form-control w-full'>
+                  <textarea
+                    name='message'
+                    rows='4'
+                    placeholder='MESSAGE'
+                    required
+                    className='w-full bg-transparent outline-none border-b border-light/40 text-light placeholder:text-light/80 text-xl pb-4 resize-none focus-within:border-light duration-500'
+                  ></textarea>
+                </div>
+                <div
+                  data-aos='zoom-in'
+                  data-aos-duration='1000'
+                  data-aos-delay='2600'
+                  data-aos-easing='ease-out-cubic'
+                  data-aos-anchor-placement='top center'
+                  className='flex items-center justify-center'
                 >
-                  Share Your Words
-                </button>
-              </div>
-            </form>
+                  <button
+                    type='submit'
+                    className='submit px-5 py-3 rounded-full text-light text-xl lg:text-2xl whitespace-nowrap bg-dark/70 hover:bg-dark/80 hover:shadow-lg duration-500 uppercase'
+                  >
+                    Share Your Words
+                  </button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
